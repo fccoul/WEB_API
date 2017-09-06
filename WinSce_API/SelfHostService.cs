265d7329840a998e3b0efa2bf52bfca6edb40674
fccoul <fccoul@gmail.com>
2017-09-06 08:58:06 +0000

    Hosting Service Windows & consumming AJAX

---
+++ WinSce_API/SelfHostService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WinSce_API
+{
+    //---creation dun simple client --->voir projet "WebApp_ConsumeAPIDicvover_forClt-->InfosWindowsSceAPI.html
+    public partial class SelfHostService : ServiceBase
+    {
+        public SelfHostService()
+        {
+            InitializeComponent();
+        }
+
+        //When the service is hosted, this OnStart event will be fired and the hosting configuration will be registered.
+        protected override void OnStart(string[] args)
+        {
+            var url = "http://localhost:5450";
+            var config = new HttpSelfHostConfiguration(url); //definition de la configuration incluant le template routing
+            
+            config.Routes.MapHttpRoute("custom",
+                                     "api/{controller}/{id}",
+                                     new { id = RouteParameter.Optional});
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config); //creation de l'hebergeur de sce API avec affectaion de la config
+            server.OpenAsync().Wait();
+
+      
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WinSce_API/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WinSce_API
+{
+    public class ServiceController : ApiController
+    {
+        public string GetString(Int32 id)
+        {
+            return "Acces Control ! pfff -this is a test - value returned through the windows service ";
+        }
+    }
+}
